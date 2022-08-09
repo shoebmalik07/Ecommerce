19a9452c39c0e2a619b2cc753ba0a6e8573b7ef6
Shoeb-0204 <malikshoaib0402@gmail.com>
2022-08-09 18:12:37 +0530

    Step 20 completed. Custom error handling. Run the following in the root folder done.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from './config/db.js'
 import colors from 'colors'
 import products from './data/products.js'
 import productRoutes from './routes/productRoutes.js'
+import {notFound , errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectDB()
@@ -15,6 +16,11 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+
+// Error Middlewares
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(process.env.PORT, () => {
