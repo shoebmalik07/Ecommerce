a91e104ab8c203cd6b3124abfc1a3f72d26b7ef0
Shoeb-0204 <malikshoaib0402@gmail.com>
2022-09-07 16:28:53 +0530

    Step 51 completed. Create the Order Screen component done.

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -21,7 +21,7 @@ export const orderCreateReducer = (state = {}, action) => {
 };
 
 export const orderDetailsReducer = (
-  state = { orderItems: [], shippingAddress: {} },
+  state = { orderItems: [], shippingAddress: {}, loading:true },
   action
 ) => {
   switch (action.type) {
