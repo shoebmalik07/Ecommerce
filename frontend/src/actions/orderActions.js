5a988f2afb987b1ca3eb7bf422cce8b82f1fbdc3
Shoeb-0204 <malikshoaib0402@gmail.com>
2022-09-07 15:48:41 +0530

    Step 50 completed. Create the order details reducer and action (frontend).

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,34 +1,63 @@
-import {ORDER_CREATE_FAIL,ORDER_CREATE_REQUEST,ORDER_CREATE_SUCCESS} from '../constants/orderConstants'
-import axios from 'axios'
+import {
+  ORDER_CREATE_FAIL,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL,
+} from "../constants/orderConstants";
+import axios from "axios";
 
-
-
-export const createOrder = (order)=> async (dispatch,getState)=>{
+export const createOrder = (order) => async (dispatch, getState) => {
   try {
-    dispatch({type: ORDER_CREATE_REQUEST})
+    dispatch({ type: ORDER_CREATE_REQUEST });
 
     const {
-      userLogin : {userInfo},
-
-    } = getState()
+      userLogin: { userInfo },
+    } = getState();
 
     const config = {
-      headers:{
-        Authorization : `Bearer ${userInfo.token}`,
-      'Content-Type': 'application/json',
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+        "Content-Type": "application/json",
       },
-    }
-    const {data} = await axios.post('/api/orders', order,config)
-    
-    dispatch({type: ORDER_CREATE_SUCCESS, payload : data})
+    };
+    const { data } = await axios.post("/api/orders", order, config);
+
+    dispatch({ type: ORDER_CREATE_SUCCESS, payload: data });
   } catch (err) {
     dispatch({
-			type: ORDER_CREATE_FAIL,
-			payload:
-				err.response && err.response.data.message
-					? err.response.data.message
-					: err.message,
-		});
-    
+      type: ORDER_CREATE_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
   }
-}
+};
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ORDER_DETAILS_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.get(`/api/orders/${id}`, config);
+    dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,7 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducer";
 import { cartReducer } from "./reducers/cartReducer";
-import {orderCreateReducer} from './reducers/orderReducers'
+import {orderCreateReducer, orderDetailsReducer} from './reducers/orderReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from "./reducers/userReducer";
 
 // localStorage.removeItem('userInfo')
@@ -20,6 +20,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile : userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails : orderDetailsReducer,
 
 });
 const cartItemsFromStorage = localStorage.getItem("cartItems")
