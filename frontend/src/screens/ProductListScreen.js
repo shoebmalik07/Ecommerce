077db3cc8aeea40c623c2b54c9bab49076b6171a
Shoeb-0204 <malikshoaib0402@gmail.com>
2022-09-17 03:01:59 +0530

    Step 64 completed. Added productList screen.

---
+++ frontend/src/screens/ProductListScreen.js
@@ -0,0 +1,104 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { IoAdd, IoPencilSharp, IoTrashBinSharp } from "react-icons/io5";
+import { useEffect } from "react";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productAction";
+
+const ProductListScreen = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listProducts());
+    } else {
+      navigate("/login");
+    }
+  }, [dispatch, navigate, userInfo]);
+
+  const createProductHandler = () => {
+    //create products
+  };
+  return (
+    <>
+      <Flex mb="5" alignItems="center" justifyContent="space-between">
+        <Heading as="h1" fontSize="3xl" mb="5">
+          Products
+        </Heading>
+        <Button onClick={createProductHandler} colorScheme="orange">
+          <Icon as={IoAdd} mr="2" fontSize="xl" fontWeight="bold" />
+          Create Products
+        </Button>
+      </Flex>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message type="error">{error}</Message>
+      ) : (
+        <Box bgColor="white" rounded="lg" shadow="lg" px="5" py="5">
+          <Table variant="striped" colorScheme="gray" size="sm">
+            <Thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>NAME</Th>
+                <Th>PRICE</Th>
+                <Th>CATEGORY</Th>
+                <Th>BRAND</Th>
+                <Th></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {products.map((product) => (
+                <Tr key={product._id}>
+                  <Td>{product._id}</Td>
+                  <Td>{product.name}</Td>
+                  <Td>{product.price}</Td>
+                  <Td>{product.category}</Td>
+                  <Td>{product.brand}</Td>
+                  <Td>
+                    <Flex justifyContent="flex-end" alignItems="center">
+                      <Button
+                        mr="4"
+                        as={RouterLink}
+                        to={`/admin/product/${product._id}/edit`}
+                        colorScheme="teal"
+                      >
+                        <Icon as={IoPencilSharp} color="white" size="sm" />
+                      </Button>
+                      <Button mr="4" colorScheme="red">
+                        <Icon as={IoTrashBinSharp} color="white" size="sm" />
+                      </Button>
+                    </Flex>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default ProductListScreen;
