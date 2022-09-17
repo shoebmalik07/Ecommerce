50b240b2f87e3ad94d85c33a3feb91258e91a357
Shoeb-0204 <malikshoaib0402@gmail.com>
2022-09-18 02:29:52 +0530

    step 69 completed. Producteditscreen done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/productEditScreen';
 
 
 
@@ -48,6 +49,7 @@ const App = () => {
           <Route path= '/admin/userlist' element = {<UserListScreen/>}/>
           <Route path= '/admin/user/:id/edit' element = {<UserEditScreen/>}/>
           <Route path= '/admin/productlist' element = {<ProductListScreen/>}/>
+          <Route path= '/admin/product/:id/edit' element = {<ProductEditScreen/>}/>
         </Routes>
       </Flex>
       <Footer />
