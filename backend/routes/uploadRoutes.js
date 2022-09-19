c802197b254927800ca144a052c5d583556dc32c
Shoeb-0204 <malikshoaib0402@gmail.com>
2022-09-19 19:23:30 +0530

    Step 70 completed. Image Upload Configuration and Endpoint done.

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,40 @@
+import express from "express";
+import multer from "multer";
+import path from "path";
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "/uploads");
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}.${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function checkFileTypes(file, cb) {
+  const filetype = /jpeg||jpg||png/;
+  const extname = filetype.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetype.test(file.mimetype);
+
+  if (extname && mimetype) {
+    cb(null, true);
+  } else {
+    cb("only images are allowed");
+  }
+}
+const upload = multer({
+  storage,
+  fileFilter: function(req,file,cb){
+    checkFileTypes(file,cb)
+  }
+})
+router.post('/', upload.single('image'), (req,res)=>{
+  res.send(`/${req.file.path}`)
+})
+
+export default router
