7c50d6489533fdfed77ffcbdeacf8d8456131543
Shoeb-0204 <malikshoaib0402@gmail.com>
2022-09-22 01:31:26 +0530

    Step 74 completed. Create Review Endpoint done.

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,15 +1,14 @@
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
 
 // @desc fetch products
 // @route GET  '/api/products'
 // @access  public
 
-const getProducts =  asyncHandler(async (req, res) => {
-  const products = await Product.find({})           // ROUTE HANDLER
-  res.json(products)
-
-})
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}); // ROUTE HANDLER
+  res.json(products);
+});
 
 // @desc fetch productById
 // @route GET '/api/products/:id'
@@ -17,65 +16,66 @@ const getProducts =  asyncHandler(async (req, res) => {
 
 const getProductById = asyncHandler(async (req, res) => {
   try {
-      const product = await Product.findById(req.params.id)
+    const product = await Product.findById(req.params.id);
 
-      if (product) {
-          res.json(product)
-      } else {
-          res.status(404).json({ messsage: 'product not found' })
-      }
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ messsage: "product not found" });
+    }
   } catch (error) {
-      console.error(error)
-      res.status(500).json({ message: 'something went wrong' })
+    console.error(error);
+    res.status(500).json({ message: "something went wrong" });
   }
-})
+});
 
 //@desc delete a product
 //@route  DELETE '/api/products/:id'
 //@access private/admin
 
-const deleteProduct = asyncHandler(async(req,res)=>{
-  const product = await Product.findById(req.params.id)
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
 
-  if(product){
-    await product.remove()
-    res.json({message: 'Product removed'})
-  }else{
-    res.status(404)
-    throw new Error('product not found')
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed" });
+  } else {
+    res.status(404);
+    throw new Error("product not found");
   }
-})
+});
 
 //@desc create a product
 //@route POST /api/products
 //access private/admin
 
-const createProduct = asyncHandler(async(req,res)=>{
+const createProduct = asyncHandler(async (req, res) => {
   const product = new Product({
-    name:'Sample product',
-    price:0,
-    user:req.user._id,
-    image:'/image/sample.jpg',
-    brand:'Sample brand',
-    category:'Sample category',
-    countInStock:0,
-    numReviews:0,
-    description:'Sample description'
-  })
+    name: "Sample product",
+    price: 0,
+    user: req.user._id,
+    image: "/image/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
 
-  const createdProduct = await product.save()
-  res.status(201).json(createdProduct)
-})
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
 
 //@desc update product
 //route PUT /api/products/:id
 //access private/admin
 
-const updateProduct = asyncHandler(async(req,res)=>{
-  const {name,price,image,description,brand,category,countInStock,} = req.body
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, image, description, brand, category, countInStock } =
+    req.body;
 
-  const product = await Product.findById(req.params.id)
-  if(product){
+  const product = await Product.findById(req.params.id);
+  if (product) {
     product.name = name;
     product.price = price;
     product.description = description;
@@ -84,14 +84,58 @@ const updateProduct = asyncHandler(async(req,res)=>{
     product.category = category;
     product.countInStock = countInStock;
 
-    const updatedProduct = await product.save()
-    res.json(updatedProduct)
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("product not found");
+  }
+});
+
+//@desc Create new Review
+//route POST/api/products/:id/reviews
+//access public
+
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviews = product.reviews.find(
+      (review) => req.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviews) {
+      res.json(400);
+      throw new Error("Product already reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: +rating,
+      comment,
+      user: req.user._id,
+    };
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+    product.rating =
+      product.reviews.reduce((acc, curr) => curr.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save()
+    res.status(201).json({message:'Review added'})
   }else{
     res.status(404)
     throw new Error('product not found')
   }
-})
+});
 
-
-
-export {getProductById, getProducts, deleteProduct,createProduct, updateProduct}
+export {
+  getProductById,
+  getProducts,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,17 +5,20 @@ import {
   getProductById,
   getProducts,
   updateProduct,
+  createProductReview
 } from "../controller/productController.js";
 import { protect, admin } from "../middleware/authMIddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route('/:id/reviews').post(protect,createProductReview)
 
 router
   .route("/:id")
   .get(getProductById)
   .delete(protect, admin, deleteProduct)
   .put(protect, admin, updateProduct);
+
 
 export default router;
